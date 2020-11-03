43bd4b16fa4e403145600e2cec4b80d99a2a74d8
Alex Aveldanez <alexsaveldanez@gmail.com>
2020-11-02 22:44:22 -0800

    added client side login feature

--- ngshop-client/src/app/core/nav-bar/nav-bar.component.ts
+++ ngshop-client/src/app/core/nav-bar/nav-bar.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
+import { IUser } from 'src/app/shared/models/user';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,11 +13,17 @@ import { IBasket } from 'src/app/shared/models/basket';
 })
 export class NavBarComponent implements OnInit {
   basket$: Observable<IBasket>;
+  currentUser$: Observable<IUser>;
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 
 }
